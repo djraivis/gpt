b2204b8d234847b979a75d3a3d95e4b4c6953343
Raivis <highdoven@gmail.com>
2023-01-01 22:45:57 +0000

    second commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from './containers';
+import { Brand, Navbar } from './components';
+import './App.css';
+
+const App = () => {
+    return (
+        <div className='App'>
+            <div className='gradient__bg'>
+                <Navbar />
+                <Header />
+            </div>
+            <Brand />
+            <WhatGPT3 />
+            <Features />
+            <Possibility />
+            <Blog />
+            <Footer />
+        </div>
+    );
+};
+
+export default App;
